874ab07b57a2bccbfc4a5223c99102ffe71f0658
MariamiGogia <mariamgogia27@gmail.com>
2024-05-21 19:29:53 +0400

    third commit

---
+++ level 03/Classwork/classwork.py
@@ -0,0 +1,16 @@
+ # task 1
+a=7
+b=0.5
+# int+float=float
+
+# task 2
+print(a+b)
+bool1=True
+bool2=False
+print(int(bool1)+int(bool2))
+#true=1 xolo false=0 amitom 1+0=1
+
+# task 3
+m = 20
+n = 24
+print(str(m) + str(n))
