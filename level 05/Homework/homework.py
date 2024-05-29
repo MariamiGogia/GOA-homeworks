1db3e3e3e0a28a50940660a0cade89584465bbf8
MariamiGogia <mariamgogia27@gmail.com>
2024-05-29 21:54:54 +0400

    fifth commit

---
+++ level 05/Homework/homework.py
@@ -0,0 +1,37 @@
+#task 1
+#We have declared Rick as the variable name, we used Snake_case 
+dog_name1 = "Rick"
+
+#We have declared John as the variable name, we used Snake_case 
+dog_name2 = "John"
+
+#We have declared Max as the variable name, we used Snake_case 
+dog_name3 = "Max"
+
+#We have used command print and concatenated two string types variables and displayed it on the screen 
+print("First dog's name is" + " " + dog_name1 + ". " + "Second dog's name is" + " " + dog_name2 + ".")
+ #We used comments to explain our codes, also Snake_case instead of space, because we know that we cannot use space in python
+
+
+#task 2
+name = "Mariami"
+a = f"My name is {name}"
+print(a)
+
+age = 14
+b = f"My age is {age}"
+print(b)
+
+singer = "Beyonce"
+c = f"Her favourite artist is {singer}"
+print(c)
+
+year = 2009
+d = f"she was born in {year}"
+print(d)
+
+subject = "German"
+e = f"Nika hates {subject}"
+print(e)
+
+
