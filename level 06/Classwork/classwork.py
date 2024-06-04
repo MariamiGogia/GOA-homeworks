2a4a018c776662080a5b82b42983582c31d27058
MariamiGogia <mariamgogia27@gmail.com>
2024-06-04 11:44:34 +0400

    sixth commit

---
+++ level 06/Classwork/classwork.py
@@ -0,0 +1,28 @@
+#We declared variable, gave it name dog_name 1 and value "Rick"
+dog_name1 = "Rick"
+
+#We eclared variable, gave it name dog_name 2 and value "John"
+dog_name2 = "John"
+
+#We reassigned variable value, gave it value "Max"
+dog_name2 = "Max"
+
+print("First dog's name is" + " " + dog_name1 + ". " + "Second dog's name is" + " " + dog_name2 + ".")
+
+
+#We have used command print and concatenated two string types variables and displayed it on the screen 
+print(f"First dog's name is {dog_name1}. Second dog's name is {dog_name2}.")
+
+
+#We used comments, Snake_case and F string to make codes read easier
+
+
+
+#task 2
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(num1 + num2)
+
+
+
+
---
+++ level 06/Homework/homework.py
@@ -0,0 +1,59 @@
+#task 2
+
+#we declared variable, gave it name num1 and used command input
+num1 = int(input("Enter first number: "))
+
+#we declared variable, gave it name num2 and used command input
+num2 = int(input("Enter second number: "))
+
+#we declared variable, gave it name num3 and used command input
+num3 = int(input("Enter third number: "))
+
+#addiction
+print(num1 + num2 + num3)
+
+#deduction
+print(num1 - num2 - num3)
+
+#multiplication
+print(num1 * num2 * num3)
+
+#division
+print(num1 / num2 / num3)
+
+
+#task 3
+
+#create account
+#we declared variable, gave it name name and used command input
+name = input("Enter your name: ")
+
+#we declared variable, gave it name surname and used command input
+surname = input("Enter your surname: ")
+
+#we declared variable, gave it name age and used command input
+age = input("Enter your age: ")
+
+#we declared variable, gave it name password and used command input
+password = input("Enter your password: ")
+
+
+#task 4
+#we used f string function
+print(f"Name: {name}")
+
+#we used f string function
+print(f"Surname: {surname}")
+
+#we used f string function
+print(f"Age: {age}")
+
+#we used f string function
+print(f"Password: {password}")
+
+
+
+
+
+
+
