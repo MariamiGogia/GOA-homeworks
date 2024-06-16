09cb1ddb2278896f24d47c85b70aee914adb29a0
MariamiGogia <mariamgogia27@gmail.com>
2024-06-16 18:08:31 +0400

    tenth commit

---
+++ level 10/Classwork/classwork.py
@@ -0,0 +1,18 @@
+#task 1
+for i in range( 50, 100 ):
+    print(i)
+
+#task 2
+for a in range( 10, 0, -1):
+    print(a)
+
+#task 3
+for b in range(100):
+    print("Hello world") 
+
+#task 4
+for i in range(1, 28):
+    print(i) 
+    print("Hello world")   
+       
+
---
+++ level 10/Homework/homework.py
@@ -0,0 +1,64 @@
+#task 2
+for i in range(0, 101, 2):
+    print(i)
+
+#task 3
+m = 1
+while m < 1000:
+    print(m)
+    m += 1
+
+#task 4
+t = 0
+n = 1
+while n < 10:
+    t += n
+    n += 1
+    print("The sum of numbers from 1 to 10 is:", t)
+
+#task 5  
+# for loop examples
+for i in range(7):
+    print("Mariami")
+
+for i in range(27, 0, -3):
+    print(i)
+
+word = "Python"
+for letter in word:
+    print(letter)
+
+for i in range(4, 40, 4):
+    print(i)
+
+b = "Snakecase"
+for letter in b:
+    print(letter)
+
+
+# while loop example
+ticket = 0
+while ticket <= 20:
+    print(ticket)
+    ticket += 1
+
+n = 7
+while n >= 3:
+    print(n) 
+    n -= 3
+
+grade = 0
+while grade <= 7:
+    print( grade )
+    grade += 1
+
+age = 10
+while age < 50:
+    print(age)
+    age += 5
+
+weight = 20
+while weight < 56:
+    print(weight)
+    weight += 2
+
