d8c62bec9b8d30b04c49b3eb6a50009e014114de
MariamiGogia <mariamgogia27@gmail.com>
2024-07-03 16:56:07 +0400

    fourteenth commit

---
+++ level 14/Classwork/classwork.py
@@ -0,0 +1,28 @@
+#task 1
+
+list = ["Mariami", 27, 3.14, "GOA", False]
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+
+
+#task 2
+list[1] = "HBD"
+list[4] = True
+print(list)
+
+
+#task 3
+list_n = ["string", False, True, 4.27, 3]
+print(list_n[-5])
+print(list_n[-4])
+print(list_n[-3])
+print(list_n[-2])
+print(list_n[-1])
+
+
+list_n[-4] = "HBD"
+list_n[-1] = True
+print(list_n)
---
+++ level 14/Homework/homework.py
@@ -0,0 +1,36 @@
+list1 = [2, 51, 11, 13, 51, 100]
+#task 1 - Output every item from list with positive indexing.
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+print(list1[5])
+
+#task 2 - Output every item from list with negative indexing.
+print(list1[-6])
+print(list1[-5])
+print(list1[-4])
+print(list1[-3])
+print(list1[-2])
+print(list1[-1])
+
+#task 3 - Replace the last element of a list with a new value.
+list1[5] = 27
+
+#task 4 - Replace the fifth element of a list with a new value.
+list1[5] = 333
+
+#task 5 - Extract the last three elements of a list.
+print(list1[3:6])
+
+#task 6 - Extract the first three elements of a list.
+print(list1[0:3])
+
+#task 7 - Extract the middle two elements of a list. ([11, 13])
+print(list1[2:4])
+
+#task 8 - Extract random elements of a list with negative indexes.
+print(list1[-6:-3])
+print(list1[-4:-2])
+print(list1[-3:-1])
