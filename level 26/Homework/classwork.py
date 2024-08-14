bd5453ace8e89631da790aa01421080a98441472
MariamiGogia <mariamgogia27@gmail.com>
2024-08-14 19:13:17 +0400

    24th commit

---
+++ level 26/Homework/classwork.py
@@ -0,0 +1,4 @@
+#task 3
+#tuple is like list, it is ordered collections of items and its immutable
+# We use it when we want to write a list, that needs to be same and can't be changed
+#It has indexing (start-0) and u can do everything except things that contains modifying them.
