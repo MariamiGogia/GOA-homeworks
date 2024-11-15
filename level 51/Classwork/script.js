785106a36fb6f345a7a6136ea2c39ef37c65dbc6
MariamiGogia <mariamgogia27@gmail.com>
2024-11-15 18:03:34 +0400

    46th commit

---
+++ level 51/Classwork/script.js
@@ -0,0 +1,21 @@
+// task 1
+// we use js to control and create movements on the website, have interactives, for example to show on the screen alert messeage
+// or make changes/movements when we click an image, make animations
+
+//task 2
+//event is specific actions that happens after we do something. for example after we click on button or moving the mouse
+
+//task 3
+//js is dynamic languge so with it we use dynamic variables, with variables we can make calculations and operations
+
+//task 4
+//when we want to create a variable we use: let, var, const
+
+
+//task 5
+//it will show an error because you can't declare the same variable (num) twice using let.
+// it should be like this: let num = 50;  num = 60   without let
+
+//task 6
+// when we need sentance to look readable in js, we start the new word's letter with uppercase, so this uppercased letters
+// looks like a humps of camel, that is why we call it CamelCase
---
+++ level 51/Homework/script.js
@@ -0,0 +1,15 @@
+//task 2
+// DOM is an object that represents the page that you see in the web browser and it structers all elements in a tree-like representation
+
+//task 3
+let num1 = 10;
+let num2 = 11;
+num2 = 17;
+console.log(num1 + num2);
+
+//task 4
+//Console is an object that provides access to the browser's debugging console. 
+//It allows you to print messages, such as errors or information.
+
+//task 5
+// let keyword is used to create/declare a variable, we can change the value of it later and is only available within the block where it's declared
