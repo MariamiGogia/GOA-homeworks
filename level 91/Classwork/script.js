13477ab6ad8ac244a8372a6dbbe0fceda642a73f
MariamiGogia <mariamgogia27@gmail.com>
2025-04-03 10:24:42 +0400

    69th commit

---
+++ level 91/Classwork/script.js
@@ -0,0 +1,37 @@
+const myForm = document.getElementById("form");
+
+myForm.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const searchPlace = document.getElementById("searchplace");
+    const apiUrl = `https://www.googleapis.com/books/v1/volumes?q=${searchPlace.value}`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const resultsDiv = document.getElementById("res");
+            resultsDiv.innerHTML = '';
+
+            data.items.forEach(item => {
+                const book = item.volumeInfo;
+                const bookDiv = document.createElement('div');
+                bookDiv.classList.add('book');
+
+                bookDiv.innerHTML = `
+                    <div class="book-details">
+                        <h3>${book.title}</h3>
+                        <p>Author: ${book.authors}</p>
+                        <p>Published Year: ${book.publishedDate}</p>
+                        <p>Description: ${book.description}</p>
+                        <p>Page Count: ${book.pageCount}</p>
+                        ${book.imageLinks && book.imageLinks.thumbnail ? `<img src="${book.imageLinks.thumbnail}" alt="${book.title}">` 
+                        : `No image of: ${book.title}`}
+                    </div>
+                `;
+                resultsDiv.appendChild(bookDiv);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
