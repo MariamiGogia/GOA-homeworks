fff4e471bc5924aa16ed4494f3da8eac07a2057e
MariamiGogia <mariamgogia27@gmail.com>
2025-05-27 18:35:08 +0400

    77th commit

---
+++ level 106/Classwork/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  return (
+     <ul>
+        <li>Mariami</li>
+        <li>Nia</li>
+        <li>Kato</li>
+        <li>Nini</li>
+        <li>Taki</li>
+     </ul>
+);
+}
+
+export default App
